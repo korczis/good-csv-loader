b690ef0d181427cbba893c5479130e16403a0798
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2014-04-10 11:26:21 -0700

    Added a post to /projects to start the interaction

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@
 
 require "bundler/setup"
 require "sinatra"
+require "securerandom"
+
+S3_ENDPOINT = "https://some_uri_here/"
 
 # Set public folder
 set :public_folder, 'public'
@@ -9,4 +12,13 @@ set :public_folder, 'public'
 # Server root asset
 get '/' do
   redirect '/index.html'
+end
+
+post '/projects' do
+  uuid = SecureRandom::uuid
+  content_type :json
+  {
+    :id => uuid,
+    :upload_files_uri => S3_ENDPOINT + uuid
+  }.to_json
 end
