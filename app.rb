8a49c66092acdd62864f3b406f95aee8745097d6
Petr Tichý <petr.tichy@gooddata.com>
2014-04-10 23:18:42 +0200

    Add S3 upload func & template

--- app.rb
+++ app.rb
@@ -7,7 +7,28 @@ require "gooddata"
 require "open-uri"
 require "pry"
 
-S3_ENDPOINT = "https://some_uri_here/"
+#S3 upload req's
+require 'base64'
+require 'openssl'
+require 'digest/sha1'
+
+def policy_document(date,uuid)
+  return "
+  {'expiration': '#{date}',
+    'conditions': [ 
+      {'bucket': 'gcl-data'}, 
+      ['starts-with', '$key', 'space-test/#{uuid}'],
+      {'acl': 'private'},
+      {'success_action_redirect': 'https://s3.amazonaws.com/gcl-data/upload-success.html'},
+      ['starts-with', '$Content-Type', ''],
+      ['content-length-range', 0, 1073741824]
+    ]
+  }"
+end
+
+aws_secret_key = 'Z56zbD0F/vR88iGN56DnGcOoagadSmKRsjOTHP/e'
+
+S3_ENDPOINT = "https://gcl-data/"
 PROJECT_CREATION_TOKEN = ENV['project_token']
 GD_LOGIN = ENV['gd_login']
 GD_PASS = ENV['gd_pass']
@@ -22,10 +43,20 @@ end
 
 post '/projects' do
   uuid = SecureRandom::uuid
+  policy = Base64.encode64(policy_document("2016-01-01T00:00:00Z",uuid)).gsub("\n","")
+  signature = Base64.encode64(
+    OpenSSL::HMAC.digest(
+        OpenSSL::Digest::Digest.new('sha1'), 
+        aws_secret_key, policy)
+    ).gsub("\n","")
+
+  project_prefix = S3_ENDPOINT + uuid
   content_type :json
   {
     :id => uuid,
-    :upload_files_uri => S3_ENDPOINT + uuid
+    :upload_files_uri => project_prefix,
+    :upload_policy => policy,
+    :upload_signature => signature
   }.to_json
 end
 
