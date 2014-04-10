93ffbe56b00e6038f37366bdfd681ebbdec015f3
Patrick McConlogue <patrick.mcconlogue@gooddata.com>
2014-04-10 13:31:12 -0700

    style changes updated UUID request, hide session button

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,13 @@ App = Ember.Application.create({
   LOG_TRANSITIONS: true,
 });
 
-// HELPERS
+// FAYE
+var fclient = new Faye.Client('/faye');
+
+
+            var send_hello = function(message) {
+                fclient.publish('/foo', {"message" : message || "hello"});
+            };
 
 App.Router.map(function() {
   this.route("index", {path: "/"});
@@ -15,15 +21,37 @@ App.IndexRoute = Ember.Route.extend({
 });
 
 App.IndexController = Ember.Controller.extend({
-  creatingConnection: true,
   status: null,
+  uuid: null,
+  files: null,
   init: function(){
-    console.log("here");
+    console.log('here');
+    var subscription = fclient.subscribe('/foo', function(message) {
+      alert(JSON.stringify(message));
+    });
   },
   actions: {
     requestUUID: function(){
       controller = this;
       controller.set('status', "Requesting UUID...");
+
+      getUUID = {
+        url: '/projects',
+        error: function(xml, err, status){
+          controller.set('status', err);
+        },
+
+        method: 'POST',
+        success: function(data){
+          controller.set('status', null);
+          controller.set('uuid', data.id);
+          controller.controllerFor('application').set('uuid', data.id);
+          controller.set('S3', data.upload_files_uri);
+          console.log(data);
+          $("#startSession").hide(30);
+        }
+      }
+      $.ajax(getUUID);
     },
   }
 
