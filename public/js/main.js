30a052beb914807c98afa3a77bae7921030f2130
Patrick McConlogue <patrick.mcconlogue@gooddata.com>
2014-04-10 11:41:17 -0700

    updated with EmberJS, added basic layout, CSS, and Handlebars lib

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,30 @@
+App = Ember.Application.create({
+  LOG_TRANSITIONS: true,
+});
 
+// HELPERS
+
+App.Router.map(function() {
+  this.route("index", {path: "/"});
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['file', 'file', 'file'];
+  },
+});
+
+App.IndexController = Ember.Controller.extend({
+  creatingConnection: true,
+  status: null,
+  init: function(){
+    console.log("here");
+  },
+  actions: {
+    requestUUID: function(){
+      controller = this;
+      controller.set('status', "Requesting UUID...");
+    },
+  }
+
+});
