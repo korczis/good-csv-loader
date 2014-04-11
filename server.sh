c4be7c9b46bd2f378a7b16bcf23ead4c7420c0a2
Adrian Toman <adrian.toman@gooddata.com>
2014-04-11 15:13:42 +0200

    removed the Server.sh script

--- server.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-export project_token='INTNA000000SRVS';export gd_pass='jindrisska';export gd_login='svarovsky+test@gooddata.com'; bundle exec rackup -s thin -E production -p 9292 config.ru -o 0.0.0.0
